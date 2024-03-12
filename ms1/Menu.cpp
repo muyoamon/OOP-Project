db2fec6c1b2aef3b3d21e1eb3faa77beb5831c8b
muyoamon <muyoamon@gmail.com>
2024-03-12 01:07:07 -0400

    added metadata

--- ms1/Menu.cpp
+++ ms1/Menu.cpp
@@ -1,3 +1,17 @@
+/* Citation and Sources...
+Final Project Milestone 1 
+Module: Menu
+Filename: Menu.cpp
+Version 1.0
+Author   Thanatorn Wongthanaporn
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2023/3/12  Preliminary release
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my workshops and assignments.
+----------------------------------------------------------- */
 #include "Menu.h"
 #include <cstdio>
 #include <cstring>
--- ms1/Time.h
+++ ms1/Time.h
@@ -1,3 +1,17 @@
+/* Citation and Sources...
+Final Project Milestone 1 
+Module: Time
+Filename: Time.h
+Version 1.0
+Author   Thanatorn Wongthanaporn
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2023/3/12  Preliminary release
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my workshops and assignments.
+----------------------------------------------------------- */
 #ifndef SENECA_TIME_H
 #define SENECA_TIME_H
 #include <iostream>
