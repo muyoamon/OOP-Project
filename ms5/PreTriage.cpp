5621d287cd6a4334eec2a95fb02ed630d34df103
muyoamon <muyoamon@gmail.com>
2024-04-09 10:06:23 -0400

    ms5 bug fixes

--- ms5/PreTriage.cpp
+++ ms5/PreTriage.cpp
@@ -191,7 +191,7 @@ namespace seneca {
       std::cout << "\n";
       std::cout << "******************************************\n";
       std::cout << "Call time: [";
-      std::cout << U.getTime();
+      std::cout << Time(U.getTime());
       std::cout << "]\n";
       std::cout << "Calling for ";
       std::cout << *m_lineup[index];
